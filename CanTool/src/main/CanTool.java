c17ff9e206a089119dea6318a9f712cacb196e25
lilinfeng8023 <398160445@qq.com>
2017-10-14 20:50:07 +0800

    commit by lilinfeng

--- CanTool/src/main/CanTool.java
+++ CanTool/src/main/CanTool.java
@@ -1,5 +1,6 @@
 package main;
 
+import serialPort.*;
 import java.awt.EventQueue;
 
 
@@ -11,6 +12,8 @@ import javax.swing.JPanel;
 import javax.swing.JTextPane;
 import javax.swing.border.TitledBorder;
 import java.awt.FlowLayout;
+import java.awt.Window;
+
 import javax.swing.JFormattedTextField;
 import java.awt.event.MouseAdapter;
 import java.awt.event.MouseEvent;
@@ -18,6 +21,7 @@ import java.awt.event.MouseEvent;
 public class CanTool {
 
 	private JFrame frmCantoolapp;
+	
 
 	/**
 	 * Launch the application.
@@ -48,21 +52,24 @@ public class CanTool {
 	private void initialize() {
 		frmCantoolapp = new JFrame();
 		frmCantoolapp.setTitle("CANToolAPP");
-		frmCantoolapp.setBounds(100, 100, 755, 597);
+		frmCantoolapp.setBounds(100, 100, 753, 611);
 		frmCantoolapp.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 		frmCantoolapp.getContentPane().setLayout(null);
 		
 		JTabbedPane tabbedPane_1 = new JTabbedPane(JTabbedPane.TOP);
-		tabbedPane_1.setBounds(14, 13, 709, 537);
+		tabbedPane_1.setBounds(15, 15, 700, 530);
 		frmCantoolapp.getContentPane().add(tabbedPane_1);
 		
 		JPanel panel_1 = new JPanel();
 		panel_1.addMouseListener(new MouseAdapter() {
 			@Override
 			public void mouseClicked(MouseEvent e) {
+				serialPort.ComSet window = new serialPort.ComSet();
+				window.frmcom.setVisible(true);	
 			}
 		});
 		tabbedPane_1.addTab("\u8BBE\u7F6ECOM\u53E3", null, panel_1, null);
+		
 		
 		JPanel panel_2 = new JPanel();
 		tabbedPane_1.addTab("\u8BBE\u7F6E\u88C5\u7F6E", null, panel_2, null);
---
+++ CanTool/src/serialPort/ComSet.java
@@ -0,0 +1,126 @@
+package serialPort;
+
+import java.awt.EventQueue;
+
+import javax.swing.JFrame;
+import javax.swing.JPanel;
+import java.awt.BorderLayout;
+import javax.swing.JTabbedPane;
+import javax.swing.JTextArea;
+import javax.swing.JLabel;
+import javax.swing.JComboBox;
+import java.awt.Font;
+import javax.swing.JButton;
+
+public class ComSet {
+
+	public JFrame frmcom;
+
+	/**
+	 * Launch the application.
+	 */
+	public static void main(String[] args) {
+		EventQueue.invokeLater(new Runnable() {
+			public void run() {
+				try {
+					ComSet window = new ComSet();
+					window.frmcom.setVisible(true);
+				} catch (Exception e) {
+					e.printStackTrace();
+				}
+			}
+		});
+	}
+
+	/**
+	 * Create the application.
+	 */
+	public ComSet() {
+		initialize();
+	}
+
+	/**
+	 * Initialize the contents of the frame.
+	 */
+	private void initialize() {
+		frmcom = new JFrame();
+		frmcom.setTitle("\u8BBE\u7F6ECOM\u53E3");
+		frmcom.setBounds(100, 100, 765, 535);
+		frmcom.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		frmcom.getContentPane().setLayout(null);
+		
+		JLabel lblNewLabel = new JLabel("Send/Receive on Comm.");
+		lblNewLabel.setFont(new Font("Calibri", Font.PLAIN, 24));
+		lblNewLabel.setBounds(56, 31, 303, 32);
+		frmcom.getContentPane().add(lblNewLabel);
+		
+		JComboBox comboBox = new JComboBox();
+		comboBox.setBounds(56, 72, 303, 32);
+		frmcom.getContentPane().add(comboBox);
+		
+		JPanel panel = new JPanel();
+		panel.setBounds(56, 128, 637, 319);
+		frmcom.getContentPane().add(panel);
+		panel.setLayout(null);
+		
+		JLabel lblNewLabel_1 = new JLabel("Com Ports Setting");
+		lblNewLabel_1.setBounds(0, 0, 292, 34);
+		panel.add(lblNewLabel_1);
+		lblNewLabel_1.setFont(new Font("Calibri", Font.PLAIN, 24));
+		
+		JLabel lblNewLabel_2 = new JLabel("Baud Rate");
+		lblNewLabel_2.setFont(new Font("Calibri", Font.PLAIN, 20));
+		lblNewLabel_2.setBounds(44, 85, 141, 34);
+		panel.add(lblNewLabel_2);
+		
+		JLabel lblNewLabel_3 = new JLabel("Parity");
+		lblNewLabel_3.setFont(new Font("Calibri", Font.PLAIN, 20));
+		lblNewLabel_3.setBounds(44, 141, 141, 34);
+		panel.add(lblNewLabel_3);
+		
+		JLabel lblNewLabel_4 = new JLabel("Parity error char.");
+		lblNewLabel_4.setFont(new Font("Calibri", Font.PLAIN, 20));
+		lblNewLabel_4.setBounds(44, 200, 141, 34);
+		panel.add(lblNewLabel_4);
+		
+		JComboBox comboBox_1 = new JComboBox();
+		comboBox_1.setBounds(201, 87, 109, 27);
+		panel.add(comboBox_1);
+		
+		JComboBox comboBox_2 = new JComboBox();
+		comboBox_2.setBounds(200, 143, 109, 27);
+		panel.add(comboBox_2);
+		
+		JComboBox comboBox_3 = new JComboBox();
+		comboBox_3.setBounds(200, 202, 109, 27);
+		panel.add(comboBox_3);
+		
+		JLabel lblNewLabel_5 = new JLabel("Data Bits");
+		lblNewLabel_5.setFont(new Font("Calibri", Font.PLAIN, 20));
+		lblNewLabel_5.setBounds(381, 90, 81, 21);
+		panel.add(lblNewLabel_5);
+		
+		JLabel lblStopBits = new JLabel("Stop Bits");
+		lblStopBits.setFont(new Font("Calibri", Font.PLAIN, 20));
+		lblStopBits.setBounds(381, 146, 81, 21);
+		panel.add(lblStopBits);
+		
+		JComboBox comboBox_4 = new JComboBox();
+		comboBox_4.setBounds(474, 87, 109, 27);
+		panel.add(comboBox_4);
+		
+		JComboBox comboBox_5 = new JComboBox();
+		comboBox_5.setBounds(474, 143, 109, 27);
+		panel.add(comboBox_5);
+		
+		JButton btnNewButton = new JButton("OK");
+		btnNewButton.setFont(new Font("Calibri", Font.PLAIN, 20));
+		btnNewButton.setBounds(296, 275, 97, 29);
+		panel.add(btnNewButton);
+		
+		JButton btnCancel = new JButton("Cancel");
+		btnCancel.setFont(new Font("Calibri", Font.PLAIN, 20));
+		btnCancel.setBounds(437, 275, 97, 29);
+		panel.add(btnCancel);
+	}
+}
