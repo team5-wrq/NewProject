9103c9e0ba45dc4d670cc7f22cd08805d99a0dea
LiyunYang2017 <1353726722@qq.com>
2017-10-25 22:06:03 +0800

    yang

---
+++ YangFunction/Book.java
@@ -0,0 +1,11 @@
+package main;
+
+public class Book {
+	private String name;
+	public Book(String name){
+		this.name = name;
+	}
+	public String getName(){
+		return name;
+	}
+}
---
+++ YangFunction/Iterator.java
@@ -0,0 +1,6 @@
+package main;
+
+public interface Iterator {
+	public abstract boolean hasNext();
+	public abstract Object next();
+}
